aa6e8104b9b905b8c3fcbe638b9cc24d0acb06c1
Ram Pasala <ram.pasala7@gmail.com>
2017-09-20 20:46:49 +0530

    DOS new line error fixed

---
+++ lib/helpers/fileHelper.ts
@@ -0,0 +1,163 @@
+import * as chalk from "chalk";
+import * as ejs from "ejs";
+import * as fs from "fs";
+import * as jsBeautify from "js-beautify";
+import * as mkdirp from "mkdirp";
+import * as path from "path";
+const tsconfig = require("../../templates/tsconfig.e2e.json");
+
+export class FileHelper {
+  public static createDirectory(filepath: string) {
+    const dirname = path.join(process.cwd(), filepath.substring(0, filepath.indexOf("*")));
+    if (!fs.existsSync(dirname)) {
+      mkdirp.sync(dirname);
+    }
+  }
+  private readonly cukeJsonFormatter: string = `json:./reports/cucumber_report.json`;
+  private beforeLaunchExpression: string;
+  private formatExpression: string;
+  private jasmineReportExpression: string;
+  private logExpression: string;
+  private onCompleteExpression: string;
+  private onPrepareExpression: string;
+  private seleniumAddressExpression: string;
+  private seleniumServerJarExpression: string;
+  private renderedFile: any;
+  private tmplFile: any;
+  private tsconfigTypes: string[] = [];
+
+  public createConfigFile(answers: any) {
+    if (answers.backend.indexOf("local machine") > -1) {
+      if (!answers.drivers && !answers.webdriver) {
+        this.seleniumServerJarExpression = `seleniumServerjar: '${answers.seleniumJar}',`;
+        this.seleniumAddressExpression = `seleniumAddress: '${answers.seleniumAddress}',`;
+      }
+      if (answers.webdriver) {
+        this.seleniumAddressExpression = `seleniumAddress: '${answers.seleniumAddress}',`;
+      }
+    } else if (answers.backend.indexOf("remote selenium server") > -1) {
+      this.seleniumAddressExpression = `seleniumAddress: '${answers.host}',`;
+    }
+    if (answers.framework === "jasmine") {
+      if (answers.jasmineTStranspiler) {
+        this.tsconfigTypes.push("jasmine", "jasminewd2");
+        this.beforeLaunchExpression = `
+                        beforeLaunch: () => {
+                             require("ts-node").register({
+                                  project: "./tsconfig.e2e.json"
+                              });
+                        },
+                        `;
+      }
+      if (answers.reportType === "spec") {
+        this.jasmineReportExpression = `const { SpecReporter } = require("jasmine-spec-reporter");`;
+        this.onPrepareExpression = `
+                        onPrepare: () => {
+                             jasmine.getEnv().addReporter(new SpecReporter({
+                                  spec: {
+                                    displayStacktrace: true
+                                  }
+                                }));
+                        },`;
+      } else if (answers.reportType === "html") {
+        this.jasmineReportExpression = `const HtmlScreenshotReporter = require("protractor-jasmine2-screenshot-reporter");`;
+        this.onPrepareExpression = `
+                        onPrepare: () => {
+                            jasmine.getEnv().addReporter(new HtmlScreenshotReporter({
+                                dest: 'target/screenshots',
+                                filename: 'protractor_jasmine_report.html'
+                              }));
+                    },`;
+      }
+    }
+    if (answers.framework === "mocha") {
+      if (answers.transpilerType === "typescript") {
+        this.tsconfigTypes.push("mocha");
+      }
+    }
+    if (answers.framework === "cucumber") {
+      if (answers.transpilerType === "typescript") {
+        this.tsconfigTypes.push("cucumber");
+      }
+      this.formatExpression = answers.cucumberReportType;
+      if (answers.cucumberReportType === "json") {
+        this.formatExpression = this.cukeJsonFormatter;
+      }
+      if (answers.cucumberReportType === "html") {
+        this.formatExpression = this.cukeJsonFormatter;
+        this.onCompleteExpression = `
+        onComplete: () => {
+            const cucumberReporterOptions = {
+                    theme: "bootstrap",
+                    jsonFile: "./reports/cucumber_report.json",
+                    output: process.cwd() + "./reports/cucumber_reporter.html",
+                    reportSuiteAsScenarios: true
+                    };
+            reporter.generate(cucumberReporterOptions);
+    },`;
+      }
+    }
+    if (answers.specPath) {
+      FileHelper.createDirectory(answers.specs);
+    }
+    if (answers.featurePath) {
+      FileHelper.createDirectory(answers.specs);
+      FileHelper.createDirectory(answers.stepDefinitions);
+    }
+    if (
+      answers.jasmineTStranspiler ||
+      answers.transpilerType === "typescript"
+    ) {
+      this.createTSconfigfile(this.tsconfigTypes);
+    }
+
+    if (answers.logging === "error") {
+      this.logExpression = `require("protractor/built/logger").Logger.logLevel = 0`;
+    } else if (answers.logging === "warn") {
+      this.logExpression = `require("protractor/built/logger").Logger.logLevel = 1`;
+    } else if (answers.logging === "debug") {
+      this.logExpression = `require("protractor/built/logger").Logger.logLevel = 3`;
+    }
+
+    if (answers.createReportPath) {
+      FileHelper.createDirectory(answers.reportPath);
+    }
+    this.tmplFile = fs.readFileSync(
+      path.join(__dirname, "../../templates/protractor.conf.ejs"),
+      "utf8"
+    );
+    this.renderedFile = ejs.render(this.tmplFile, {
+      answers,
+      beforeLaunchExpression: this.beforeLaunchExpression,
+      formatExpression: this.formatExpression,
+      jasmineReportExpression: this.jasmineReportExpression,
+      logExpression: this.logExpression,
+      onCompleteExpression: this.onCompleteExpression,
+      onPrepareExpression: this.onPrepareExpression,
+      seleniumAddressExpression: this.seleniumAddressExpression,
+      seleniumServerJarExpression: this.seleniumServerJarExpression,
+    });
+    fs.writeFileSync(
+      path.join(process.cwd(), "./protractor.conf.js"),
+      jsBeautify(this.renderedFile)
+    );
+    console.log(
+      `
+${chalk.green("Configuration file was created successfully!")}
+${chalk.green("To run your tests, execute:")}
+
+${chalk.green("$ protractor protractor.conf.js")}
+        `
+    );
+  }
+
+  private createTSconfigfile(options: string[]) {
+    tsconfig.compilerOptions.types = tsconfig.compilerOptions.types.concat(
+      options
+    );
+    fs.writeFileSync(
+      path.join(process.cwd(), "./tsconfig.e2e.json"),
+      jsBeautify(JSON.stringify(tsconfig))
+    );
+  }
+}
---
+++ lib/helpers/questions.ts
@@ -0,0 +1,249 @@
+const browsers = ["chrome", "firefox", "internet explorer"];
+const cloudServices = ["sauce labs", "browserstack"];
+const frameworks = ["jasmine", "mocha", "cucumber"];
+const logType = ["info", "error", "warn", "debug"];
+const reportTypes = ["dot", "spec", "json", "html"];
+const cucumberReportTypes = ["progress", "summary", "json", "html"];
+const transpilers = ["typescript", "coffee-script"];
+
+const questions: any[] = [
+  {
+    default: "http://localhost",
+    message: "What is the base url of your application?",
+    name: "baseURL",
+    type: "input",
+  },
+  {
+    choices: [
+      "My local machine",
+      "In cloud services like Sauce Labs or Browserstack",
+      "I know a remote selenium server",
+    ],
+    default: "My local machine",
+    message: "What is your test environment?",
+    name: "backend",
+    type: "list",
+  },
+  {
+    default: "http://127.0.0.1:4444/wd/hub",
+    message: "What is the host address of that remote server?",
+    name: "host",
+    type: "input",
+    when: (answers: any) => answers.backend.indexOf("remote selenium") > -1,
+  },
+  {
+    choices: cloudServices,
+    default: "sauce labs",
+    message: "Please select the could service: ",
+    name: "services",
+    type: "list",
+    when: (answers: any) => answers.backend.indexOf("In cloud services ") > -1,
+  },
+  {
+    default: "SAUCE_USERNAME",
+    message: "Environment variable for sauce labs username",
+    name: "sauceUser",
+    type: "input",
+    when: (answers: any) => answers.services === "sauce labs",
+  },
+  {
+    default: "SAUCE_ACCESS_KEY",
+    message: "Environment variable for sauce labs access key",
+    name: "sauceKey",
+    type: "input",
+    when: (answers: any) => answers.services === "sauce labs",
+  },
+  {
+    default: "BROWSER_STACK_USERNAME",
+    message: "Environment variable for Browserstack username",
+    name: "browserstackUser",
+    type: "input",
+    when: (answers: any) => answers.services === "browserstack,"
+  },
+  {
+    default: "BROWSER_STACK_ACCESS_KEY",
+    message: "Environment variable for Browserstack access key",
+    name: "browserstackKey",
+    type: "input",
+    when: (answers: any) => answers.services === "browserstack",
+  },
+  {
+    default: true,
+    message: "Do you want to use webdriver-manager's drivers?",
+    name: "webdriver",
+    type: "confirm",
+    when: (answers: any) => answers.backend.indexOf("local machine") > -1,
+  },
+  {
+    default: false,
+    message: "Do you want to use built in browser drivers?",
+    name: "drivers",
+    type: "confirm",
+    when: (answers: any) =>
+      !answers.webdriver && answers.backend.indexOf("local machine") > -1,
+  },
+  {
+    default: "./selenium/*.jar",
+    message: "Please enter the selenium jar file location:",
+    name: "seleniumJar",
+    type: "input",
+    when: (answers: any) =>
+      !answers.drivers &&
+      !answers.webdriver &&
+      answers.backend.indexOf("local machine") > -1,
+  },
+  {
+    default: "http://localhost:4444/wd/hub",
+    message: "Please enter the local selenium server address:",
+    name: "seleniumAddress",
+    type: "input",
+    when: (answers: any) =>
+      !answers.drivers &&
+      answers.backend.indexOf("local machine") > -1,
+  },
+  {
+    choices: browsers,
+    default: "chrome",
+    message: "Select the choice of your browser:",
+    name: "browser",
+    type: "list",
+  },
+  {
+    choices: frameworks,
+    default: "jasmine",
+    message: "Which framework do you want to use?",
+    name: "framework",
+    type: "list",
+  },
+  {
+    default: true,
+    message: "Do you want to use typescript transpiler?",
+    name: "jasmineTStranspiler",
+    type: "confirm",
+    when: (answers: any) => answers.framework === "jasmine",
+  },
+  {
+    default: true,
+    message: "Do you want to use a transpiler?",
+    name: "transpiler",
+    type: "confirm",
+    when: (answers: any) => answers.framework.match(/(mocha|cucumber)/),
+  },
+  {
+    choices: transpilers,
+    default: "typescript",
+    message: "Please select the transpiler:",
+    name: "transpilerType",
+    type: "list",
+    when: (answers: any) => answers.transpiler,
+  },
+  {
+    default: (answers: any) => {
+      if (
+        answers.jasmineTStranspiler ||
+        answers.transpilerType === "typescript"
+      ) {
+        return "./test/specs/**/*.ts";
+      } else if (answers.transpilerType === "coffee-script") {
+        return "./test/specs/**/*.coffee";
+      } else {
+        return "./test/specs/**/*.js";
+      }
+    },
+    message: "Where are your test specs located?",
+    name: "specs",
+    type: "input",
+    when: (answers: any) => answers.framework.match(/(jasmine|mocha)/),
+  },
+  {
+    default: true,
+    message: (answers: any) => {
+      return `Do you want to create ${answers.specs.substring(0, answers.specs.indexOf("*"))} folder?`;
+    },
+    name: "specPath",
+    type: "confirm",
+    when: (answers: any) => answers.framework.match(/(jasmine|mocha)/),
+
+  },
+  {
+    default: "./features/**/*.feature",
+    message: "Where are your feature files located?",
+    name: "specs",
+    type: "input",
+    when: (answers: any) => answers.framework === "cucumber",
+  },
+  {
+    default: (answers: any) => {
+      if (answers.transpilerType === "typescript") {
+        return "./features/stepDefinitions/**/*.ts";
+      } else if (answers.transpilerType === "coffee-script") {
+        return "./features/stepDefinitions/**/*.coffee";
+      } else {
+        return "./features/stepDefinitions/**/*.js";
+      }
+    },
+    message: "Where are your step definitions located?",
+    name: "stepDefinitions",
+    type: "input",
+    when: (answers: any) => answers.framework === "cucumber",
+  },
+  {
+    default: true,
+    message: "Do you want to create the above folders?",
+    name: "featurePath",
+    type: "confirm",
+    when: (answers: any) => answers.framework === "cucumber",
+
+  },
+  {
+    choices: logType,
+    default: "info",
+    message: "Level of logging verbosity:",
+    name: "logging",
+    type: "list",
+  },
+  {
+    default: true,
+    message: "Do you want to generate reports of your tests?",
+    name: "reports",
+    type: "confirm",
+  },
+  {
+    choices: reportTypes,
+    default: "dot",
+    message: "Please select the type of report:",
+    name: "reportType",
+    type: "list",
+    when: (answers: any) =>
+      answers.reports && answers.framework.match(/(jasmine|mocha)/),
+  },
+  {
+    choices: cucumberReportTypes,
+    default: "progress",
+    message: "Please select the type of report:",
+    name: "cucumberReportType",
+    type: "list",
+    when: (answers: any) => answers.reports && answers.framework === "cucumber",
+  },
+  {
+    default: "./reports",
+    message: "Where do you want to store your reports?",
+    name: "reportPath",
+    type: "input",
+    when: (answers: any) => answers.reports,
+  },
+  {
+    default: true,
+    message: (answers: any) => {
+      return `Do you want to create ${answers.reportPath} folder?`;
+    },
+    name: "createReportPath",
+    type: "confirm",
+    when: (answers: any) => answers.reports,
+  },
+];
+
+/**
+ * Public Inteface
+ */
+export { questions };
