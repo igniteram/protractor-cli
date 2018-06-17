3e29646e0a694fae69ede5bc683d12cebff78376
Ram Pasala <ram.pasala7@gmail.com>
2018-06-17 23:20:15 +0530

    feat(install modules): added npm progress & async installation (#27)
    replaced all sync functions to async for faster execution
    
    BREAKING CHANGE: removed cross-spawn dependency

--- lib/helpers/fileHelper.ts
+++ lib/helpers/fileHelper.ts
@@ -4,104 +4,148 @@ import * as fs from 'fs';
 import * as jsBeautify from 'js-beautify';
 import * as mkdirp from 'mkdirp';
 import * as path from 'path';
-const eslint = require('../../templates/.eslintrc.json');
 const tsconfig = require('../../templates/tsconfig.e2e.json');
-const tslint = require('../../templates/tslint.json');
+import {promisify} from 'util';
 
-export class FileHelper {
-  public static createDirectory(filepath: string) {
-    if (filepath.indexOf('*') > -1) {
-      filepath = filepath.substring(0, filepath.indexOf('*'));
-    }
-    const dirname = path.join(process.cwd(), filepath);
-    if (!fs.existsSync(dirname)) {
-      mkdirp.sync(dirname);
-    }
+const mkdirpAsync = promisify(mkdirp);
+const readFileAsync = promisify(fs.readFile.bind(fs));
+const writeFileAsync = promisify(fs.writeFile.bind(fs));
+
+const cukeJsonFormatter: string = `json:./reports/cucumber_report.json`;
+let beforeLaunchExpression: string;
+let formatExpression: string;
+let jasmineReportExpression: string;
+let logExpression: string;
+let mochaReportExpression: string;
+let onCompleteExpression: string;
+let onPrepareExpression: string;
+let seleniumAddressExpression: string;
+let seleniumServerJarExpression: string;
+let transpilerExpression: string;
+const tsconfigTypes: string[] = [];
+
+function existsAsync(filePath: string) {
+  let fulfill: any = null;
+  const promise = new Promise((x) => {
+    fulfill = x;
+  });
+  fs.access(filePath, (err) => {
+    fulfill(!err);
+  });
+  return promise;
+}
+
+async function createDirectory(filepath: string) {
+  if (filepath.indexOf('*') > -1) {
+    filepath = filepath.substring(0, filepath.indexOf('*'));
   }
-  private readonly cukeJsonFormatter: string = `json:./reports/cucumber_report.json`;
-  private beforeLaunchExpression: string;
-  private formatExpression: string;
-  private jasmineReportExpression: string;
-  private logExpression: string;
-  private mochaReportExpression: string;
-  private onCompleteExpression: string;
-  private onPrepareExpression: string;
-  private renderedFile: any;
-  private seleniumAddressExpression: string;
-  private seleniumServerJarExpression: string;
-  private transpilerExpression: string;
-  private tmplFile: any;
-  private tsconfigTypes: string[] = [];
-
-  public createConfigFile(answers: any) {
-    if (answers.backend.indexOf('local machine') > -1) {
-      if (!answers.drivers && !answers.webdriver) {
-        this.seleniumServerJarExpression = `seleniumServerjar: '${answers.seleniumJar}',`;
-        this.seleniumAddressExpression = `seleniumAddress: '${answers.seleniumAddress}',`;
-      }
-      if (answers.webdriver) {
-        this.seleniumAddressExpression = `seleniumAddress: '${answers.seleniumAddress}',`;
-      }
-    } else if (answers.backend.indexOf('remote selenium server') > -1) {
-      this.seleniumAddressExpression = `seleniumAddress: '${answers.host}',`;
+  const dirname = path.join(process.cwd(), filepath);
+  try {
+    if (!(await existsAsync(dirname))) {
+      mkdirpAsync(dirname);
     }
-    if (answers.framework === 'jasmine') {
-      if (answers.transpilerType === 'typescript') {
-        this.tsconfigTypes.push('jasmine', 'jasminewd2');
-        this.beforeLaunchExpression = `require('ts-node').register({
+  } catch (err) {
+    throw new Error(err.toString());
+  }
+}
+
+async function createTSconfigfile(options: string[]) {
+  tsconfig.compilerOptions.types = tsconfig.compilerOptions.types.concat(options);
+  try {
+    await writeFileAsync(
+        path.join(process.cwd(), './tsconfig.e2e.json'), JSON.stringify(tsconfig, null, 4));
+  } catch (err) {
+    throw new Error(err.toString());
+  }
+}
+
+async function createTSLintFile() {
+  try {
+    await writeFileAsync(
+        path.join(process.cwd(), './tslint.json'), JSON.stringify(tsconfig, null, 4));
+  } catch (err) {
+    throw new Error(err.toString());
+  }
+}
+
+async function createESLintRCFile() {
+  try {
+    await writeFileAsync(
+        path.join(process.cwd(), './.eslintrc.json'), JSON.stringify(tsconfig, null, 4));
+  } catch (err) {
+    throw new Error(err.toString());
+  }
+}
+
+async function createConfigFile(answers: any) {
+  if (answers.backend.indexOf('local machine') > -1) {
+    if (!answers.drivers && !answers.webdriver) {
+      seleniumServerJarExpression = `seleniumServerjar: '${answers.seleniumJar}',`;
+      seleniumAddressExpression = `seleniumAddress: '${answers.seleniumAddress}',`;
+    }
+    if (answers.webdriver) {
+      seleniumAddressExpression = `seleniumAddress: '${answers.seleniumAddress}',`;
+    }
+  } else if (answers.backend.indexOf('remote selenium server') > -1) {
+    seleniumAddressExpression = `seleniumAddress: '${answers.host}',`;
+  }
+  if (answers.framework === 'jasmine') {
+    if (answers.transpilerType === 'typescript') {
+      tsconfigTypes.push('jasmine', 'jasminewd2');
+      beforeLaunchExpression = `require('ts-node').register({
             project: './tsconfig.e2e.json'
          });`;
-      }
-      if (answers.reportType === 'spec') {
-        this.jasmineReportExpression = `const {SpecReporter} = require('jasmine-spec-reporter');`;
-        this.onPrepareExpression = `jasmine.getEnv().addReporter(new SpecReporter({
+    }
+    if (answers.reportType === 'spec') {
+      jasmineReportExpression = `const {SpecReporter} = require('jasmine-spec-reporter');`;
+      onPrepareExpression = `jasmine.getEnv().addReporter(new SpecReporter({
             spec: {
               displayStacktrace: true
             }
           }));
           `;
-      } else if (answers.reportType === 'html') {
-        this.jasmineReportExpression =
-            `const HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');`;
-        this.onPrepareExpression = `jasmine.getEnv().addReporter(new HtmlScreenshotReporter({
+    } else if (answers.reportType === 'html') {
+      jasmineReportExpression =
+          `const HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');`;
+      onPrepareExpression = `jasmine.getEnv().addReporter(new HtmlScreenshotReporter({
             dest: 'target/screenshots',
             filename: 'protractor_jasmine_report.html'
           }));
               `;
-      }
     }
-    if (answers.framework === 'mocha') {
-      if (answers.transpilerType === 'typescript') {
-        this.tsconfigTypes.push('mocha');
-        this.transpilerExpression = `'ts:ts-node/register'`;
-      } else if (answers.transpilerType === 'coffeescript') {
-        this.transpilerExpression = `'coffee:coffeescript/register'`;
-      }
-      if (answers.reportType === 'html') {
-        this.mochaReportExpression = `'mochawesome',
+  }
+  if (answers.framework === 'mocha') {
+    if (answers.transpilerType === 'typescript') {
+      tsconfigTypes.push('mocha');
+      transpilerExpression = `'ts:ts-node/register'`;
+    } else if (answers.transpilerType === 'coffeescript') {
+      transpilerExpression = `'coffee:coffeescript/register'`;
+    }
+    if (answers.reportType === 'html') {
+      mochaReportExpression = `'mochawesome',
         reporterOptions : {
             reportDir: './reports',
             reportFileName: 'protractor_mocha_report',
             enableCharts: true
         }`;
-      } else {
-        this.mochaReportExpression = answers.reportType;
-      }
+    } else {
+      mochaReportExpression = answers.reportType;
     }
-    if (answers.framework === 'cucumber') {
-      if (answers.transpilerType === 'typescript') {
-        this.tsconfigTypes.push('cucumber');
-        this.transpilerExpression = `'ts:ts-node/register'`;
-      } else if (answers.transpilerType === 'coffeescript') {
-        this.transpilerExpression = `'coffee:coffeescript/register'`;
-      }
-      this.formatExpression = answers.cucumberReportType;
-      if (answers.cucumberReportType === 'json') {
-        this.formatExpression = this.cukeJsonFormatter;
-      }
-      if (answers.cucumberReportType === 'html') {
-        this.formatExpression = this.cukeJsonFormatter;
-        this.onCompleteExpression = `const cucumberReporterOptions = {
+  }
+  if (answers.framework === 'cucumber') {
+    if (answers.transpilerType === 'typescript') {
+      tsconfigTypes.push('cucumber');
+      transpilerExpression = `'ts:ts-node/register'`;
+    } else if (answers.transpilerType === 'coffeescript') {
+      transpilerExpression = `'coffee:coffeescript/register'`;
+    }
+    formatExpression = answers.cucumberReportType;
+    if (answers.cucumberReportType === 'json') {
+      formatExpression = cukeJsonFormatter;
+    }
+    if (answers.cucumberReportType === 'html') {
+      formatExpression = cukeJsonFormatter;
+      onCompleteExpression = `const cucumberReporterOptions = {
                   theme: 'bootstrap',
                   jsonFile: './reports/cucumber_report.json',
                   output: process.cwd() + './reports/cucumber_reporter.html',
@@ -109,72 +153,68 @@ export class FileHelper {
                   };
           reporter.generate(cucumberReporterOptions);
     `;
-      }
     }
+  }
+  try {
     if (answers.specPath) {
-      FileHelper.createDirectory(answers.specs);
+      await createDirectory(answers.specs);
     }
     if (answers.featurePath) {
-      FileHelper.createDirectory(answers.specs);
-      FileHelper.createDirectory(answers.stepDefinitions);
+      await createDirectory(answers.specs);
+      await createDirectory(answers.stepDefinitions);
     }
     if (answers.transpilerType === 'typescript') {
-      this.createTSconfigfile(this.tsconfigTypes);
+      await createTSconfigfile(tsconfigTypes);
     }
     if (answers.linter === 'tslint') {
-      this.createTSLintFile();
+      await createTSLintFile();
     }
     if (answers.linter === 'eslint-plugin-protractor') {
-      this.createESLintRCFile();
+      await createESLintRCFile();
     }
-    if (answers.logging === 'error') {
-      this.logExpression = answers.logging.toUpperCase();
-    } else if (answers.logging === 'warn') {
-      this.logExpression = answers.logging.toUpperCase();
-    } else if (answers.logging === 'debug') {
-      this.logExpression = answers.logging.toUpperCase();
-    } else {
-      this.logExpression = 'INFO';
-    }
-    if (answers.createReportPath) {
-      FileHelper.createDirectory(answers.reportPath);
-    }
-    this.tmplFile =
-        fs.readFileSync(path.join(__dirname, '../../templates/protractor.conf.ejs'), 'utf8');
-    this.renderedFile = ejs.render(this.tmplFile, {
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  if (answers.logging === 'error') {
+    logExpression = answers.logging.toUpperCase();
+  } else if (answers.logging === 'warn') {
+    logExpression = answers.logging.toUpperCase();
+  } else if (answers.logging === 'debug') {
+    logExpression = answers.logging.toUpperCase();
+  } else {
+    logExpression = 'INFO';
+  }
+  if (answers.createReportPath) {
+    await createDirectory(answers.reportPath);
+  }
+
+  try {
+    const templateData =
+        await readFileAsync(path.join(__dirname, '../../templates/protractor.conf.ejs'), 'utf8');
+    const renderedFile = ejs.render(templateData, {
       answers,
-      beforeLaunchExpression: this.beforeLaunchExpression,
-      formatExpression: this.formatExpression,
-      jasmineReportExpression: this.jasmineReportExpression,
-      logExpression: this.logExpression,
-      mochaReportExpression: this.mochaReportExpression,
-      onCompleteExpression: this.onCompleteExpression,
-      onPrepareExpression: this.onPrepareExpression,
-      seleniumAddressExpression: this.seleniumAddressExpression,
-      seleniumServerJarExpression: this.seleniumServerJarExpression,
-      transpilerExpression: this.transpilerExpression,
+      beforeLaunchExpression,
+      formatExpression,
+      jasmineReportExpression,
+      logExpression,
+      mochaReportExpression,
+      onCompleteExpression,
+      onPrepareExpression,
+      seleniumAddressExpression,
+      seleniumServerJarExpression,
+      transpilerExpression,
     });
-    fs.writeFileSync(
-        path.join(process.cwd(), './protractor.conf.js'), jsBeautify(this.renderedFile));
+    await writeFileAsync(
+        path.join(process.cwd(), './protractor.conf.js'), jsBeautify(renderedFile));
     console.log(`
 ${chalk.green('Configuration file was created successfully!')}
 ${chalk.green('To run your tests, execute:')}
-
 ${chalk.green('$ protractor protractor.conf.js')}
-        `);
-  }
-
-  private createTSconfigfile(options: string[]) {
-    tsconfig.compilerOptions.types = tsconfig.compilerOptions.types.concat(options);
-    fs.writeFileSync(
-        path.join(process.cwd(), './tsconfig.e2e.json'), JSON.stringify(tsconfig, null, 4));
-  }
-
-  private createTSLintFile() {
-    fs.writeFileSync(path.join(process.cwd(), './tslint.json'), JSON.stringify(tslint, null, 4));
-  }
-
-  private createESLintRCFile() {
-    fs.writeFileSync(path.join(process.cwd(), './.eslintrc.json'), JSON.stringify(eslint, null, 4));
+`);
+  } catch (err) {
+    throw new Error(err.toString());
   }
 }
+
+export {existsAsync, createConfigFile};
